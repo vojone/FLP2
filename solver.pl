9ac13cf42bda23e06f6aa61b27cdb5a4844f5b2e
vojone <vvd1@centrum.cz>
2024-04-27 21:22:34 +0200

    Adapted solver to the new cube.pl interface

--- solver.pl
+++ solver.pl
@@ -1,70 +1,132 @@
+/**
+* flp-23-log
+* solver.pl
+*
+* The main body and arguments parsing of rubik's cube solver. 
+*
+* Author: Vojtech Dvorak (xdvora3o)
+*/
+
 :- consult("cube").
 :- consult("ids_solver").
 
-% The mock cube for the testing
-cube(C) :- C = [
-        [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 ],
-        [9 , 10, 11, 12, 13, 14, 15, 16, 17],
-        [18, 19, 20, 21, 22, 23, 24, 25, 26],
-        [27, 28, 29, 30, 31, 32, 33, 34, 35],
-        [36, 37, 38, 39, 40, 41, 42, 43, 44],
-        [45, 46, 47, 48, 49, 50, 51, 52, 53]
-    ].
+
+/**               Clauses for checking the state of a cube                  **/
+
+
+% side_color(color, cube)
+% Checks if one side of the cube has specified color (all squares must have
+% the color)
+side_color(_, []) :- !.
+side_color(COLOR, [COLOR|T]) :- side_color(COLOR, T).
+
+
+% cube_done(cube)
+% Check if cube is solved
+% Note: only this solution is accepted:
+% 555
+% 555
+% 555
+% 111 222 333 444
+% 111 222 333 444
+% 111 222 333 444
+% 666
+% 666
+% 666
+cube_done([]) :- !. 
+cube_done([FRONT, RIGHT, BACK, LEFT, TOP, BOT]) :-
+    side_color(1, FRONT),
+    side_color(2, RIGHT),
+    side_color(3, BACK),
+    side_color(4, LEFT),
+    side_color(5, TOP),
+    side_color(6, BOT).
+
+
+
+% Auxiliary clauses for testing
 
 cube_s(C) :- C = [
-    [3,3,3,3,5,5,5,5,5],
-    [4,4,4,6,6,6,6,6,6],
-    [5,5,5,3,3,5,3,3,3],
-    [4,4,4,4,4,4,6,6,6],
-    [1,1,1,2,2,2,2,2,2],
-    [1,1,1,1,1,1,2,2,2]
-].
-
-done_cube(C) :- C = [
-        [1,1,3,1,1,3,1,1,3],
-        [4,4,4,2,2,2,2,2,2],
-        [1,1,1,3,3,3,3,3,3],
-        [2,2,2,4,4,4,4,4,4],
+        [2,2,2,1,1,1,4,4,4],
+        [3,3,3,2,2,2,1,1,1],
+        [4,4,4,3,3,3,2,2,2],
+        [1,1,1,4,4,4,3,3,3],
         [5,5,5,5,5,5,5,5,5],
         [6,6,6,6,6,6,6,6,6]
     ].
 
+done_cube(C) :- C = [
+        [1,1,1,1,1,1,1,1,1],
+        [2,2,2,2,2,2,2,2,2],
+        [3,3,3,3,3,3,3,3,3],
+        [4,4,4,4,4,4,4,4,4],
+        [5,5,5,5,5,5,5,5,5],
+        [6,6,6,6,6,6,6,6,6]
+    ].
 
-write_solutionm_([]).
-write_solutionm_([(HMOVE, HCUBE)|TMOVES]) :- 
+scramble(C, SC) :-
+    move_seq([move_r_cw, move_s_cw, move_e_ccw, move_d_ccw, move_b_cw], C, _, _, SC).
+
+
+/**               Clauses for writign a solution to stdout                  **/
+
+
+% write_moves_(moves)
+% Writes list with moves to stdout
+write_moves_([]).
+write_moves_([HMOVE|T]) :- write(HMOVE), write(" "), write_moves_(T).
+
+% write_moves(moves)
+% Writes list with moves to stdout in reversed order (individual moves are
+% appended when moves are performed, therefore this is correct order)
+write_moves(MOVES) :- reverse(MOVES, RMOVES), write_moves_(RMOVES), write("\n").
+
+
+% smove(cube, move_sign, result_cube)
+% Performs move base on sign
+smove(C, S, RC) :- sign2move(S, MOVE_CLAUSE), call(MOVE_CLAUSE, C, _, _, RC).
+
+
+% write_solutionm_(initial_cube, moves)
+% Writes solution (with moves!) to stdout
+write_solutionm_(_, []).
+write_solutionm_(CUBE, [HMOVE|TMOVES]) :- 
     write(HMOVE), write("\n"),
-    write_cube(HCUBE), write("\n"),
-    write_solutionm_(TMOVES).
+    smove(CUBE, HMOVE, RCUBE),
+    write_cube(CUBE), write("\n"),
+    write_solutionm_(RCUBE, TMOVES).
 
-write_solution_([]).
-write_solution_([(_, HCUBE)|TMOVES]) :- write_cube(HCUBE), write("\n"), write_solution_(TMOVES).
-write_solution(MOVES, ARGV) :- reverse(MOVES, RMOVES),
+
+% write_solutionm_(initial_cube, moves)
+% Writes solution (WITHOUT moves!) to stdout
+write_solution_(_, []).
+write_solution_(CUBE, [HMOVE|TMOVES]) :-
+    smove(CUBE, HMOVE, RCUBE),
+    write_cube(RCUBE),
+    write("\n"),
+    write_solution_(RCUBE, TMOVES).
+write_solution(INITIAL_CUBE, MOVES, ARGV) :- 
+    reverse(MOVES, RMOVES),
+    write_cube(INITIAL_CUBE), % Write initial cube first
+    write("\n"),
     (
-        memberchk('-v', ARGV) -> 
-            write_solutionm_(RMOVES);
-            write_solution_(RMOVES)
+        memberchk('-v', ARGV) -> % Write solution (with or without moves)
+            write_solutionm_(INITIAL_CUBE, RMOVES);
+            write_solution_(INITIAL_CUBE, RMOVES)
     ).
 
 
-% scramble(FC) :-
-%     done_cube(C),
-%     move_u_ccw(C, _, _, RC),
-%     move_e_cw(R___, _, _, R____),
-%     move_m_ccw(R____, _, _, R_____).
-
-% solve(INIT_CUBE, WC_MOVES2) :-
-%     solve_correctly_sided(INIT_CUBE, [], CS_MOVES, CS_CUBE, 0),
-%     solve_daisy(CS_CUBE, CS_MOVES, WC_MOVES1, WC_CUBE1, 100),
-%     solve_white_cross(WC_CUBE1, WC_MOVES1, WC_MOVES2, WC_CUBE2, 100).
-
+% solve(intial_cube, moves)
+% Treíes to solve the cube and return (reversed) sequence of moves
 solve(INIT_CUBE, MOVES) :-
-    solve_ids(INIT_CUBE, MOVES, _, 0, 20).
+    solve_ids(INIT_CUBE, cube_done, 20, MOVES). % All cubes are solvable until 20 moves
 
 
+% Main body
 main(ARGV) :-
     read_cube(CUBE, NL),
     solve(CUBE, MOVES),
-    write_solution(MOVES, ARGV),
+    write_solution(CUBE, MOVES, ARGV),
     (
         memberchk('-v', ARGV) -> 
             write_moves(MOVES);
