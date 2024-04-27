2ad426d1de46ef8b3e76ccf48f3c842c5a2b9f8b
vojone <vvd1@centrum.cz>
2024-04-27 16:56:03 +0200

    Reworked ids_solver

--- ids_solver.pl
+++ ids_solver.pl
@@ -2,9 +2,6 @@
 
 :- dynamic(ids_state_/1).
 
-write_moves_([]).
-write_moves_([(HMOVE, _)|T]) :- write(HMOVE), write(" "), write_moves_(T).
-write_moves(MOVES) :- reverse(MOVES, RMOVES), write_moves_(RMOVES), write("\n").
 
 check_ids_state(C) :- ids_state_(C), !, false.
 check_ids_state(_).
@@ -13,29 +10,54 @@ save_ids_state(C) :- asserta(ids_state_(C)).
 remove_ids_state(C) :- retract(ids_state_(C)).
 clear_ids_states :- retractall(ids_state_(_)).
 
+
+ids_move(C, M, M_, RC_) :-
+    move_u_cw(C, M, M_, RC_);
+    move_u_ccw(C, M, M_, RC_);
+    move_d_cw(C, M, M_, RC_);
+    move_d_ccw(C, M, M_, RC_);
+    move_r_cw(C, M, M_, RC_);
+    move_r_ccw(C, M, M_, RC_);
+    move_l_cw(C, M, M_, RC_);
+    move_l_ccw(C, M, M_, RC_);
+    move_f_cw(C, M, M_, RC_);
+    move_f_ccw(C, M, M_, RC_);
+    move_b_cw(C, M, M_, RC_);
+    move_b_ccw(C, M, M_, RC_);
+    move_m_cw(C, M, M_, RC_);
+    move_m_ccw(C, M, M_, RC_);
+    move_e_cw(C, M, M_, RC_);
+    move_e_ccw(C, M, M_, RC_);
+    move_s_cw(C, M, M_, RC_);
+    move_s_ccw(C, M, M_, RC_).
+
+
 /** Complete solution by IDS **/
-solve_ids_step(_, _, _, _, D) :-  D =< 0, !, false.
-solve_ids_step(C, M, M, C, _) :- cube_done(C), !.
-solve_ids_step(C, M, RM, RC, D) :- move_u_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_u_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_d_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_d_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_r_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_r_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_l_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_l_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_f_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_f_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_b_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_b_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_m_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_m_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_e_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_e_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_s_cw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
-solve_ids_step(C, M, RM, RC, D) :- move_s_ccw(C, M, M_, RC_), check_ids_state(RC_), save_ids_state(RC_), ND is D - 1, solve_ids_step(RC_, M_, RM, RC, ND).
+solve_ids_step(C, C, M, M, DONE_C, _, _) :- call(DONE_C, C), !. % If cube satisfies "done condition", stop and return the cube as the result 
+solve_ids_step(_, _, _, _, _, D, MD) :-  D >= MD, !, false. % If current max depth is lower than current depth, stop with false
+solve_ids_step(C, RC, M, RM, DC, D, MD) :- ids_move(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_u_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_u_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_d_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_d_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_r_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_r_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_l_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_l_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_f_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_f_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_b_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_b_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_m_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_m_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_e_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_e_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_s_cw(C, M, M_, RC_),  ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
+% solve_ids_step(C, RC, M, RM, DC, D, MD) :- move_s_ccw(C, M, M_, RC_), ND is D + 1, solve_ids_step(RC_, RC, M_, RM, DC, ND, MD).
 
 
-solve_ids(C, RM, RC, D, _) :- save_ids_state(C), solve_ids_step(C, [], RM, RC, D), !.
-solve_ids(C, RM, RC, D, MD) :- D =< MD, ND is D + 1, clear_ids_states, solve_ids(C, RM, RC, ND, MD).
+solve_ids(C, RM, DONE_C, D, _) :- solve_ids_step(C, _, [], RM, DONE_C, 0, D), !.
+solve_ids(C, RM, DONE_C, D, MD) :- D < MD, ND is D + 1, solve_ids(C, RM, DONE_C, ND, MD).
 
+
+solve_ids(C, DONE_CLAUSE, MD, RM) :- solve_ids(C, RM, DONE_CLAUSE, 0, MD).
